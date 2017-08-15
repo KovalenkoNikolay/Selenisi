901115e9205a8febe66de00018be24b3d9ee58e5
User <User@USER-PC>
2017-08-15 23:30:14 +0300

    Add project files.

---
+++ Selenisi/Browser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenisi
+{
+    public static class Browser
+    {
+        public static void Open(string url)
+        {
+            IWebDriver driver = new ChromeDriver();
+            WebDriverStore.WebDriver = driver;
+            WebDriverStore.WebDriver.Navigate().GoToUrl(url);
+        }
+
+        public static void Close()
+        {
+            WebDriverStore.WebDriver.Close();
+        }
+    }
+}
---
+++ Selenisi/WebDriverStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenisi
+{
+    class WebDriverStore
+    {
+        public static IWebDriver WebDriver { get; set; }
+
+        public static void InitWebDriver()
+        {
+            WebDriver = new ChromeDriver();
+        }
+    }
+}
